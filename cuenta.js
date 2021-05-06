cbf5d817fad2eed3359af1f1beae1147c5ab1962
cmaradiaga22 <62809557+cmaradiaga22@users.noreply.github.com>
2021-05-06 00:23:20 -0600

    Add files via upload

---
+++ cuenta.js
@@ -0,0 +1,20 @@
+function operaciones(op)
+{
+var suma = [5,12,33,45,34,24,65,76,43]
+var multiplicar = [5,12,33,45,34,24,65,76,43]
+var a=5;
+var b=12;
+var c=33;
+var d=45;
+var e=34;
+var f=24;
+var g=65;
+var h=76;
+var i=43;
+
+suma= a+b+c+d+e+f+g+h+i;
+multiplicar=a*b*c*d*e*f*g*h*i;
+
+console.log(suma);
+console.log(multiplicar);  
+}
